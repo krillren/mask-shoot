9f3535ed121d3324b9f4d614cd4ebdda3a67c437
Lucien Martial <lucienmartial33@gmail.com>
2026-01-31 14:39:52 +0100

    feat : mask logic in GameManager

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,7 +11,16 @@ public class GameManagerScript : MonoBehaviour
 
     public List<Mask> AllMask;
     private List<Mask> targetMask;
-    
+
+
+
+    public float maskIntroInterval = 30f;
+
+    private List<Mask> activeMaskPool = new();
+    private Dictionary<Mask, int> alivePerMask = new();
+
+    private Coroutine maskRoutine;
+
 
     private int aliveEntities;
 
@@ -23,6 +33,82 @@ public class GameManagerScript : MonoBehaviour
         }
 
         Instance = this;
+    }
+
+    private void Start()
+    {
+        maskRoutine = StartCoroutine(MaskProgressionRoutine());
+    }
+
+    private IEnumerator MaskProgressionRoutine()
+    {
+        yield return new WaitForSeconds(1f);
+
+        AddRandomMaskToPool();
+
+        while (true)
+        {
+            yield return new WaitForSeconds(maskIntroInterval);
+            AddRandomMaskToPool();
+        }
+    }
+
+    private void AddRandomMaskToPool()
+    {
+        List<Mask> available = AllMask.FindAll(m => !activeMaskPool.Contains(m));
+
+        if (available.Count == 0)
+            return;
+
+        Mask newMask = available[Random.Range(0, available.Count)];
+
+        activeMaskPool.Add(newMask);
+        alivePerMask[newMask] = 0;
+
+        Debug.Log($"New mask added to pool: {newMask.maskName}");
+    }
+
+    public Mask GetRandomActiveMask()
+    {
+        if (activeMaskPool.Count == 0)
+            return null;
+
+        return activeMaskPool[Random.Range(0, activeMaskPool.Count)];
+    }
+
+    public void RegisterMaskInstance(Mask mask)
+    {
+        if (mask == null) return;
+
+        if (!alivePerMask.ContainsKey(mask))
+            alivePerMask[mask] = 0;
+
+        alivePerMask[mask]++;
+    }
+    private void KillCharacter(CharacterMask character)
+    {
+        Mask mask = character.equippedMask;
+
+        if (mask != null && alivePerMask.ContainsKey(mask))
+        {
+            alivePerMask[mask]--;
+
+            if (alivePerMask[mask] <= 0)
+            {
+                RemoveMaskFromGame(mask);
+            }
+        }
+
+        UnregisterEntity(character.gameObject);
+        Destroy(character.gameObject);
+    }
+
+    private void RemoveMaskFromGame(Mask mask)
+    {
+        activeMaskPool.Remove(mask);
+        alivePerMask.Remove(mask);
+
+        Debug.Log($"Mask removed from game: {mask.maskName}");
     }
 
     public void RegisterEntity(GameObject entity)
@@ -51,12 +137,6 @@ public class GameManagerScript : MonoBehaviour
         {
             KillCharacter(character);
         }
-    }
-
-    private void KillCharacter(CharacterMask character)
-    {
-        UnregisterEntity(character.gameObject);
-        Destroy(character.gameObject);
     }
 
     public void EndGame()
